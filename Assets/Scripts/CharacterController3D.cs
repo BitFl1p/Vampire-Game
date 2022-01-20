e9db754a65785e90fa5adb71545e2ea51fc5ed0a
Lumbr 990 <jumiciobi@gmail.com>
2022-01-20 23:16:35 +0000

    smoothened animations out

--- Assets/Scripts/CharacterController3D.cs
+++ Assets/Scripts/CharacterController3D.cs
@@ -50,9 +50,10 @@ public class CharacterController3D : MonoBehaviour
     {
         Vector2 lastDir = direction;
         direction = input.Player.Move.ReadValue<Vector2>();
-        rb.velocity = (transform.forward * direction.y + transform.right * direction.x) * speed;
-        Vector2 smoothDirection = Vector2.Lerp(lastDir.normalized, direction.normalized, 1);
-        anim.SetFloat("X", smoothDirection.normalized.x);
-        anim.SetFloat("Y", smoothDirection.normalized.y);
+        rb.velocity = rb.velocity + (transform.forward * direction.y + transform.right * direction.x) * speed;
+        rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -speed * 5, speed * 5), rb.velocity.y, Mathf.Clamp(rb.velocity.z, -speed * 5, speed * 5));
+        Vector2 localVelocity = new Vector2(Vector3.Dot(rb.velocity, transform.right), Vector3.Dot(rb.velocity, transform.forward));
+        anim.SetFloat("X", localVelocity.x);
+        anim.SetFloat("Y", localVelocity.y);
     }
 }
